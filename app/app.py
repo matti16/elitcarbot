2145a19f8a48a831f585bf4a9e8662b0356c4992
Mattia BRUSAMENTO (contractor) <Mattia.BRUSAMENTO@amadeus.com>
2018-07-22 15:34:08 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,45 @@
+import requests
+import json
+from flask import Flask, request
+
+from . import chatbot
+
+# FB messenger credentials
+ACCESS_TOKEN = "EAAGwOQvq9J4BAL8iDnEVUpkfZBuY2pgkuLjD8aXxkF4LZAz8pEPtir96ZBJIHXiq3ZCUfLMd8ZAjbVQEmZCCkPJAeCL0GW9yT24W6E797pz4LTK68luVTSNiBXFbkZBh3v93rOKkHFqKReStnAZBOOAmzoOB6l5SmDJ47X6yHgL8tgZDZD"
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    # our endpoint echos back the 'hub.challenge' value specified when we setup the webhook
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == 'foo':
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return 'Hello World (from Flask!)', 200
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    response = chatbot.response(message)
+
+    if response:
+        reply(sender, response)
+
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/chatbot.py
@@ -0,0 +1,76 @@
+import pickle
+import json
+import tflearn
+import tensorflow as tf
+import numpy as np
+import re
+import random
+
+import nltk
+from nltk.stem.snowball import ItalianStemmer
+
+stemmer = ItalianStemmer()
+
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+
+# import our chat-bot intents file
+with open('intents.json') as json_data:
+    intents = json.load(json_data)
+
+
+tf.reset_default_graph()
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+model.load('model/model.tflearn')
+
+
+ERROR_THRESHOLD=0.25
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(re.sub(r'[^\w\s]', ' ', sentence), language='italian')
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return np.array(bag)
+
+def classify(sentence, show_details=False):
+    results = model.predict([bow(sentence, words, show_details)])[0]
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+
+    if show_details: print(return_list)
+    return return_list
+
+
+def response(sentence, show_details=False):
+    results = classify(sentence, show_details=show_details)
+    if results:
+        for i in intents['intents']:
+            if i['intent'] == results[0][0]:
+                return random.choice(i['responses'])
+    
